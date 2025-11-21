1b5047c0b88dda8c53343e6e273cd3a28d87494e
Lena Le <ll224ve@student.lnu.se>
2025-11-21 15:24:57 +0100

    Add test for BugWrongMessage

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,7 +1,7 @@
 
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
+// import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
@@ -15,11 +15,12 @@ import { Password } from '../src/BugDoesNotHash'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
-    const emptyPassword = '';
+    const tooShortPassword = 'abc123'
+    const correctMessage = 'Too short password'
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+    test('constructor Should Throw Correct Message for Too Short Password', () => {
+        expect(() => new Password(tooShortPassword)).toThrow(correctMessage)
+    })
 
     //Add your tests here
-});
+})
