650b42bc99fb6e53b3d4041b0d1075d916aa6241
Lena Le <ll224ve@student.lnu.se>
2025-11-24 16:54:30 +0100

    Add test for BugDoesNotHash

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,7 +1,7 @@
 
 // Select one of the Password versions to test
 
-// import { Password } from '../src/BugDoesNotHash'
+// import { Password } from '../src/BugDoesNotHash' //✅
 // import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
@@ -11,25 +11,32 @@
 // import { Password } from '../src/BugVeryShort' //✅
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage' //✅
-import { Password } from '../src/Correct'
+// import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
     const tooShortPassword = 'abcde123456'
     const correctMessage = 'Too short password'
     const passwordContainingNumber = 'abcdefgh1234'
+    const nonHashedPassword = 'sunooluvr123'
 
     // fails BugWrongMessage + BugVeryShort, BugToShortPassword, bugNEverContainsNumbers
     it('constructor Should Throw Correct Message for Too Short Password', () => {
         expect(() => new Password(tooShortPassword)).toThrow(correctMessage)
     })
 
-    // failsBugVeryShort & BugTOShortPassword
+    // failsBugVeryShort & BugToShortPassword
     // to-do
 
     // fails BugNeverContainsNumbers
     it('constructor Should Not Throw for Password Containing Number', () => {
         expect(() => new Password(passwordContainingNumber)).not.toThrow()
     })
-    
+
+    // fails BugDoesNotHash
+    it('password Should Not Equal Input', () => {
+        const pwd = new Password(nonHashedPassword)
+        expect(pwd.getPasswordHash()).not.toBe(nonHashedPassword)
+    })
+
 })
