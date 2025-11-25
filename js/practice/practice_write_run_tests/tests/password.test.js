716a04194b1823ad04bb681f785e024db64017fb
Lena Le <ll224ve@student.lnu.se>
2025-11-25 20:10:46 +0100

    Fix test for BugisPasswordAlwaysSame to expect false

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -3,7 +3,7 @@
 
 // import { Password } from '../src/BugDoesNotHash' //✅
 // import { Password } from '../src/BugDoesNotTrim' //✅
- import { Password } from '../src/BugisPasswordAlwaysSame'
+// import { Password } from '../src/BugisPasswordAlwaysSame' //✅
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
 // import { Password } from '../src/BugNeverContainsNumbers' //✅
@@ -11,7 +11,7 @@
 // import { Password } from '../src/BugVeryShort' //✅
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage' //✅
-// import { Password } from '../src/Correct'
+ import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
@@ -50,7 +50,7 @@ describe('Password class, test suite', () => {
     it('isPasswordSame Should Return False For Different Passwords', () => {
         const passwordA = new Password('heeseungluvr123')
         const passwordB = new Password('jayluvr123')
-        expect.passwordA.isPasswordSame(passwordB).toBe(true)
+        expect(passwordA.isPasswordSame(passwordB)).toBe(false)
     })
 
 })
