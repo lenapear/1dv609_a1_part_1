90bf58cbb94ff0bd599e31d40ee5f008533142dc
Lena Le <ll224ve@student.lnu.se>
2025-11-29 00:34:46 +0100

    Add test for BuggySSNHelperAllowDayUpTo30

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,16 +1,22 @@
 //import { SSNHelper } from '../src/correct/SSNHelper'
-// import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat' //✅
+//import { SSNHelper } from '../src/bugs/BuggySSNHelperIncorrectFormat' //✅
+import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30'
 
 
 
 describe('SSNHelper Tests', () => {
 
     // fails BuggySSNHelperIncorrectFormat
-    test('incorrectSSNFormat should return false for incorrect format input', () => {
+    it('incorrectSSNFormat should return false for incorrect format input', () => {
         const sut = new SSNHelper()
         const incorrectSSNFormat = '0306247777'
-        expect(sut.isCorrectFormat(incorrectSSNFormat)).toBe(false);
-    });
+        expect(sut.isCorrectFormat(incorrectSSNFormat)).toBe(false)
+    })
 
-    //Add your tests here
-});
+    // fails BuggySSNHelperAllowDayUpTo30
+    it('isValidDay should return true for days up until 31', () => {
+        const sut = new SSNHelper()
+        const dayString = '31'
+        expect(sut.isValidDay(dayString)).toBe(true)
+    })
+})
